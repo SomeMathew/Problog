2ec1836a0eacd2d4e86d22ae00d9db670298d5e5
ilklin <ilklin@gmail.com>
2018-11-03 15:28:59 -0400

    Query processing logic flow

--- Problog/src/edu/comp6591/problog/Program.java
+++ Problog/src/edu/comp6591/problog/Program.java
@@ -1,5 +1,6 @@
 package edu.comp6591.problog;
 
+import abcdatalog.ast.PositiveAtom;
 import edu.comp6591.problog.util.FileHelper;
 import edu.comp6591.problog.util.ASTHelper;
 import edu.comp6591.problog.validation.ProblogProgram;
@@ -17,7 +18,21 @@ public class Program {
 			String data = FileHelper.getFile(path);
 			ProblogProgram program = ASTHelper.getProgram(data);
 			
+			// DO FIXPOINT CALCULATION HERE
 			
+			System.out.println("The data is processed and ready to use. Please enter your query (or 0 to stop):");
+			String query = scan.nextLine();
+			
+			while(!query.equals("0")) {
+				PositiveAtom goal = ASTHelper.getGoal(query);
+				
+				// DO QUERY EVALUATION HERE
+				
+				System.out.println("OUTPUT RESULTS HERE");
+				
+				System.out.println("Please enter your query (or 0 to stop):");
+				query = scan.nextLine();
+			}
 		}
 		catch(Exception ex) {
 			System.out.println(ex.getMessage());
