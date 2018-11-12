b654c1a6bd24c929c2e92d1473695bfd3306f3ca
SomeMatthew <someMathew@outlook.com>
2018-11-12 14:10:41 -0500

    Indexing of facts constant position to speed up unification.
    This is not currently used.

--- Problog/src/edu/comp6591/problog/datastructure/FactsIndex.java
+++ Problog/src/edu/comp6591/problog/datastructure/FactsIndex.java
@@ -1,15 +1,20 @@
 package edu.comp6591.problog.datastructure;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import abcdatalog.ast.Constant;
+import abcdatalog.ast.PositiveAtom;
 import abcdatalog.ast.PredicateSym;
 import abcdatalog.ast.Term;
+import abcdatalog.ast.visitors.TermVisitor;
+import abcdatalog.ast.visitors.TermVisitorBuilder;
 
 /**
  * Data structure to index Predicates and the position of constants in a set of
@@ -20,6 +25,10 @@ public class FactsIndex {
 
 	public FactsIndex() {
 		this.index = new HashMap<>();
+	}
+
+	public void addFacts(Collection<AtomKey> facts) {
+		facts.forEach(this::addFact);
 	}
 
 	public void addFact(AtomKey fact) {
@@ -46,10 +55,28 @@ public class FactsIndex {
 		Set<AtomKey> facts = null;
 		Map<ConstantPosition, Set<AtomKey>> consPosMap = index.get(predicate);
 		if (consPosMap != null) {
-			facts = constantPositions.stream().filter(consPosMap::containsKey)
-					.flatMap((key) -> consPosMap.get(key).stream()).collect(Collectors.toSet());
+			
+			
+			List<Set<AtomKey>> factsNonIntersected = constantPositions.stream().filter(consPosMap::containsKey)
+					.map((key) -> consPosMap.get(key)).collect(Collectors.toList());
 		}
 		return facts;
+	}
+
+	public Set<AtomKey> retrieve(PositiveAtom atom) {
+		List<ConstantPosition> positions = new LinkedList<>();
+
+		Term[] args = atom.getArgs();
+		TermVisitor<Void, Constant> visitor = new TermVisitorBuilder<Void, Constant>()
+				.onConstant((cons, unused) -> cons).orNull();
+		for (int i = 0; i < args.length; i++) {
+			Constant c = args[i].accept(visitor, null);
+			if (c != null) {
+				positions.add(new ConstantPosition(i, c));
+			}
+		}
+
+		return retrieve(atom.getPred(), positions);
 	}
 
 	public static class ConstantPosition {
