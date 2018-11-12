77950b283861685151b96db8eaf04e68a349632f
SomeMatthew <someMathew@outlook.com>
2018-11-12 01:32:35 -0500

    Change to the Parameter functions to act on lists of Certainty.

--- Problog/src/edu/comp6591/problog/engine/ProblogEngineBase.java
+++ Problog/src/edu/comp6591/problog/engine/ProblogEngineBase.java
@@ -1,6 +1,8 @@
 package edu.comp6591.problog.engine;
 
 import edu.comp6591.problog.util.ParameterHelper;
+
+import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -8,35 +10,38 @@ import java.util.function.Function;
  * Base class defining common methods and variables for all Problog engines
  */
 public abstract class ProblogEngineBase implements IProblogEngine {
-	
-	public static final Function<Set<Double>, Double> disjunctionParam = ParameterHelper::independence;
-	public static final Function<Set<Double>, Double> conjunctionParam = ParameterHelper::minimum;
-	public static final Function<Set<Double>, Double> propagationParam = ParameterHelper::product;
-	
+
+	public static final Function<List<Double>, Double> disjunctionParam = ParameterHelper::independence;
+	public static final Function<List<Double>, Double> conjunctionParam = ParameterHelper::minimum;
+	public static final Function<List<Double>, Double> propagationParam = ParameterHelper::product;
+
 	/**
 	 * Apply and execute independence function for disjunction parameter
+	 * 
 	 * @param uncertainties
 	 * @return calculated uncertainty
 	 */
-	public static Double disjunction(Set<Double> uncertainties) {
+	public static Double disjunction(List<Double> uncertainties) {
 		return disjunctionParam.apply(uncertainties);
 	}
-	
+
 	/**
 	 * Apply and execute minimum function for conjunction parameter
+	 * 
 	 * @param uncertainties
 	 * @return calculated uncertainty
 	 */
-	public static Double conjunction(Set<Double> uncertainties) {
+	public static Double conjunction(List<Double> uncertainties) {
 		return conjunctionParam.apply(uncertainties);
 	}
-	
+
 	/**
 	 * Apply and execute product function for propagation parameter
+	 * 
 	 * @param uncertainties
 	 * @return calculated uncertainty
 	 */
-	public static Double propagation(Set<Double> uncertainties) {
+	public static Double propagation(List<Double> uncertainties) {
 		return propagationParam.apply(uncertainties);
 	}
 }
--- Problog/src/edu/comp6591/problog/util/ParameterHelper.java
+++ Problog/src/edu/comp6591/problog/util/ParameterHelper.java
@@ -1,47 +1,51 @@
 package edu.comp6591.problog.util;
 
+import java.util.List;
 import java.util.Set;
 
 /**
  * Provide methods to use as parameter function
  */
 public class ParameterHelper {
-	
+
 	/**
 	 * Calculate union of probabilities of list of independent clauses
+	 * 
 	 * @param list
 	 * @return final uncertainty
 	 */
-	public static Double independence(Set<Double> list) {
+	public static Double independence(List<Double> list) {
 		double result = 0;
-		for(Double probability : list) {
+		for (Double probability : list) {
 			result += probability - (result * probability);
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Calculate minimum probability of list of clauses
+	 * 
 	 * @param list
 	 * @return final uncertainty
 	 */
-	public static Double minimum(Set<Double> list) {
+	public static Double minimum(List<Double> list) {
 		double result = 0;
-		for(Double probability : list) {
+		for (Double probability : list) {
 			result = result < probability ? result : probability;
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Calculate product of probabilities of list of clauses
+	 * 
 	 * @param list
 	 * @return final uncertainty
 	 */
-	public static Double product(Set<Double> list) {
+	public static Double product(List<Double> list) {
 		double result = 0;
 		double identity = 1; // Used avoid returning 1 for empty list
-		for(Double probability : list) {
+		for (Double probability : list) {
 			identity *= probability;
 			result = identity;
 		}
