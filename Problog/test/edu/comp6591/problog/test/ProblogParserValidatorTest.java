c4a73ae189d47386621d076f50043dc2d7dce15f
SomeMatthew <someMathew@outlook.com>
2018-11-17 01:34:13 -0500

    Test modified for the new AST.

--- Problog/test/edu/comp6591/problog/test/ProblogParserValidatorTest.java
+++ Problog/test/edu/comp6591/problog/test/ProblogParserValidatorTest.java
@@ -1,11 +1,11 @@
 package edu.comp6591.problog.test;
 
-import abcdatalog.ast.Clause;
-import abcdatalog.ast.PositiveAtom;
 import abcdatalog.parser.DatalogTokenizer;
+import edu.comp6591.problog.ast.Atom;
+import edu.comp6591.problog.ast.Clause;
 import edu.comp6591.problog.parser.ProblogParseException;
 import edu.comp6591.problog.util.ASTHelper;
-import edu.comp6591.problog.validation.ProblogProgramOLD;
+import edu.comp6591.problog.validation.IProblogProgram;
 import edu.comp6591.problog.validation.ProblogValidationException;
 import java.util.Set;
 import org.junit.Test;
@@ -14,20 +14,21 @@ import org.junit.Test;
  * Testing parsing and validation of various types of input sets
  */
 public class ProblogParserValidatorTest {
-	
+
 	String program;
-	ProblogProgramOLD validProgram;
+	IProblogProgram validProgram;
 	DatalogTokenizer t;
 	Set<Clause> ast;
-	PositiveAtom goal;
-	
+	Atom goal;
+
 	public ProblogParserValidatorTest() {
 	}
-	
+
 	/**
 	 * Check parsing of probabilities
+	 * 
 	 * @throws ProblogParseException
-	 * @throws ProblogValidationException 
+	 * @throws ProblogValidationException
 	 */
 	@Test
 	public void ProbabilitiesTest() throws ProblogParseException, ProblogValidationException {
@@ -35,11 +36,12 @@ public class ProblogParserValidatorTest {
 				+ "tc(X,Y) :- edge(X,Z), tc(Z,Y) : 0.25. cycle :- tc(X,X) : 0.3.";
 		validProgram = ASTHelper.getProgram(program);
 	}
-	
+
 	/**
 	 * Check multiset for rules with different probability for same head
+	 * 
 	 * @throws ProblogParseException
-	 * @throws ProblogValidationException 
+	 * @throws ProblogValidationException
 	 */
 	@Test
 	public void DifferentHeadProbabilitiesTest() throws ProblogParseException, ProblogValidationException {
@@ -47,11 +49,12 @@ public class ProblogParserValidatorTest {
 				+ "tc(X,Y) :- edge(X,Z), tc(Z,Y) : 0.25. cycle :- tc(X,X) : 0.3.";
 		validProgram = ASTHelper.getProgram(program);
 	}
-	
+
 	/**
 	 * Check validation of probability constraint
+	 * 
 	 * @throws ProblogParseException
-	 * @throws ProblogValidationException 
+	 * @throws ProblogValidationException
 	 */
 	@Test(expected = ProblogValidationException.class)
 	public void ProbabilityConstraintTest() throws ProblogParseException, ProblogValidationException {
@@ -59,23 +62,24 @@ public class ProblogParserValidatorTest {
 				+ "tc(X,Y) :- edge(X,Z), tc(Z,Y) : 0.25. cycle :- tc(X,X) : 0.3.";
 		validProgram = ASTHelper.getProgram(program);
 	}
-	
+
 	/**
 	 * Check normal datalog program
+	 * 
 	 * @throws ProblogParseException
-	 * @throws ProblogValidationException 
+	 * @throws ProblogValidationException
 	 */
 	@Test
 	public void NormalProgramTest() throws ProblogParseException, ProblogValidationException {
-		program = "edge(0,1). edge(1,2). tc(X,Y) :- edge(X,Y)." 
-				+ "tc(X,Y) :- edge(X,Z), tc(Z,Y). cycle :- tc(X,X).";
+		program = "edge(0,1). edge(1,2). tc(X,Y) :- edge(X,Y)." + "tc(X,Y) :- edge(X,Z), tc(Z,Y). cycle :- tc(X,X).";
 		validProgram = ASTHelper.getProgram(program);
 	}
-	
+
 	/**
 	 * Check negated atom not allowed
+	 * 
 	 * @throws ProblogParseException
-	 * @throws ProblogValidationException 
+	 * @throws ProblogValidationException
 	 */
 	@Test(expected = ProblogValidationException.class)
 	public void DisallowNegatedAtomTest() throws ProblogParseException, ProblogValidationException {
@@ -83,11 +87,12 @@ public class ProblogParserValidatorTest {
 				+ "reachable(X,Y) :- lk_1(X,Z), %ignore\n not reachable(Z,Y).";
 		validProgram = ASTHelper.getProgram(program);
 	}
-	
+
 	/**
 	 * Check goals
+	 * 
 	 * @throws ProblogParseException
-	 * @throws ProblogValidationException 
+	 * @throws ProblogValidationException
 	 */
 	@Test
 	public void GoalsTest() throws ProblogParseException, ProblogValidationException {
