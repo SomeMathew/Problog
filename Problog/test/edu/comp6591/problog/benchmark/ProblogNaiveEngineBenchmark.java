6bd98d0b6adbde884701d9b390c4a21d35f1d6ab
ilklin <ilklin@gmail.com>
2018-12-01 15:42:25 -0500

    Benchmark test results - wip

--- Problog/test/edu/comp6591/problog/benchmark/ProblogNaiveEngineBenchmark.java
+++ Problog/test/edu/comp6591/problog/benchmark/ProblogNaiveEngineBenchmark.java
@@ -1,6 +1,5 @@
-package edu.comp6591.problog.test;
+package edu.comp6591.problog.benchmark;
 
-import edu.comp6591.problog.datastructure.Statistics;
 import edu.comp6591.problog.engine.IProblogEngine;
 import edu.comp6591.problog.engine.ProblogEngineException;
 import edu.comp6591.problog.engine.ProblogEngineFactory;
@@ -13,26 +12,35 @@ import java.io.File;
 import java.io.IOException;
 import org.junit.Test;
 
-public class ProblogSemiNaiveEngineBenchmark {
-	    
+public class ProblogNaiveEngineBenchmark {
+	
 	private String program;
 	private IProblogProgram validProgram;
 	private IProblogEngine engine;
-	private Statistics stats;
-	private int threshold = 500;
-
+	private final int threshold = 500;
+	
 	@Test
 	public void BenchmarkTest() throws ProblogParseException, ProblogValidationException, ProblogEngineException, IOException {
 		
 		File folder = new File(".\\Benchmark\\input");
 		File[] list = folder.listFiles();
 		
+		// Warmup
+		for (int i = 0; i < threshold; i++) {
+			program = FileHelper.getFile(folder + "\\clique10_0.5.pl");
+			validProgram = ASTHelper.getProgram(program);
+			engine = ProblogEngineFactory.createEngine(ProblogEngineFactory.Mode.Naive, validProgram);
+			engine.init(validProgram);
+		}
+		
 		for (File benchmark : list) {
 			program = FileHelper.getFile(benchmark.getPath());
 			validProgram = ASTHelper.getProgram(program);
-			engine = ProblogEngineFactory.createEngine(ProblogEngineFactory.Mode.SemiNaive, validProgram);
+			engine = ProblogEngineFactory.createEngine(ProblogEngineFactory.Mode.Naive, validProgram);
 			engine.init(validProgram);
 			System.out.println(benchmark.getName() +"\n" + engine.getStats());
+			FileHelper.setFile(".\\Benchmark\\output\\" + benchmark.getName().replace(".pl", "_naive.pl"), 
+				ASTHelper.printFacts(engine.getComputedDatabase()));
 		}
 	}
 }
